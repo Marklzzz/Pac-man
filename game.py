b85cfc4ee81fea9a6a3685db31d4654d4d5ebcf1
Mark1zzz <ermack228@yandex.ru>
2021-01-14 13:37:18 +0300

    более удобное масштабирование

--- game.py
+++ game.py
@@ -5,29 +5,30 @@ pygame.init()
 class Field:
     def __init__(self):
         size = 720, 720
+        size_of_parts = 24
         screen = pygame.display.set_mode(size)
         self.angle_1 = pygame.image.load('data/mazeparts/угол карты ↑←.png')
-        self.angle_1 = pygame.transform.scale(self.angle_1, (24, 24))
+        self.angle_1 = pygame.transform.scale(self.angle_1, (size_of_parts, size_of_parts))
         screen.blit(self.angle_1, (0, 0))
 
         for i in range(1, 10):
             wall = pygame.image.load('data/mazeparts/борт карты ↑.png')
-            wall = pygame.transform.scale(wall, (24, 24))
-            screen.blit(wall, (i * 24, 0))
+            wall = pygame.transform.scale(wall, (size_of_parts, size_of_parts))
+            screen.blit(wall, (i * size_of_parts, 0))
 
-        self.spicific_angle_1 = pygame.transform.scale(pygame.image.load('data/mazeparts/поворот внутри карты ↑→.png'), (24, 24))
-        screen.blit(self.spicific_angle_1, (240, 0))
+        self.spicific_angle_1 = pygame.transform.scale(pygame.image.load('data/mazeparts/поворот внутри карты ↑→.png'), (size_of_parts, size_of_parts))
+        screen.blit(self.spicific_angle_1, (size_of_parts * 10, 0))
 
-        self.spicific_angle_2 = pygame.transform.scale(pygame.image.load('data/mazeparts/поворот внутри карты ↑←.png'), (24, 24))
-        screen.blit(self.spicific_angle_2, (264, 0))
+        self.spicific_angle_2 = pygame.transform.scale(pygame.image.load('data/mazeparts/поворот внутри карты ↑←.png'), (size_of_parts, size_of_parts))
+        screen.blit(self.spicific_angle_2, (size_of_parts * 11, 0))
 
         for i in range(1, 10):
             wall = pygame.image.load('data/mazeparts/борт карты ↑.png')
-            wall = pygame.transform.scale(wall, (24, 24))
-            screen.blit(wall, (264 + i * 24, 0))
+            wall = pygame.transform.scale(wall, (size_of_parts, size_of_parts))
+            screen.blit(wall, (size_of_parts * 11 + i * size_of_parts, 0))
 
-        self.angle_2 = pygame.transform.scale(pygame.image.load('data/mazeparts/угол карты ↑→.png'), (24, 24))
-        screen.blit(self.angle_2, (504, 0))
+        self.angle_2 = pygame.transform.scale(pygame.image.load('data/mazeparts/угол карты ↑→.png'), (size_of_parts, size_of_parts))
+        screen.blit(self.angle_2, (size_of_parts * 21, 0))
         pygame.display.flip()
         while pygame.event.wait().type != pygame.QUIT:
             pass
